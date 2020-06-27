4b595275561b3ac8a7451414f5c405dad4c291a2
Fatih <fatihgul@gmail.com>
2020-06-27 11:55:36 -0400

    fill the form

--- src/Project11.java
+++ src/Project11.java
@@ -1,9 +1,14 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
+import java.util.Random;
 
 public class Project11 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "D:\\BrowserDriver\\chromedriver.exe");
         WebDriver driver = new ChromeDriver(); // this will open the chrome browser
         driver.get("http://a.testaddressbook.com/sign_in");
@@ -13,7 +18,38 @@ public class Project11 {
         driver.findElement(By.id("session_password")).sendKeys("asdf1234");
         driver.findElement(By.cssSelector("input[value='Sign in']")).click();
         driver.findElement(By.cssSelector("a[href='/addresses']")).click();
+        Thread.sleep(1000);
         driver.findElement(By.cssSelector("a[href='/addresses/new']")).click();
-        address_first_name
+        Thread.sleep(1000);
+        driver.findElement(By.id("address_first_name")).sendKeys("Fernando");
+         driver.findElement(By.id("address_last_name")).sendKeys("Torres");
+        driver.findElement(By.id("address_street_address")).sendKeys("Anfield Rd");
+        driver.findElement(By.id("address_secondary_address")).sendKeys("Anfield");
+        driver.findElement(By.id("address_city")).sendKeys("Liverpool");
+
+        //Randomly select hte state
+        WebElement state = driver.findElement(By.id("address_state"));
+        Select select =new Select(state);
+        List<WebElement> stateList = select.getOptions();
+        Random rand = new Random();
+        select.selectByIndex(rand.nextInt(stateList.size()));
+
+
+        driver.findElement(By.id("address_zip_code")).sendKeys("L4 0TH");
+        driver.findElement(By.id("address_country_us")).click();
+        driver.findElement(By.id("address_birthday")).sendKeys("03201984");
+
+        driver.findElement(By.id("address_age")).sendKeys("36");
+        driver.findElement(By.id("address_website")).sendKeys("https://www.google.com/");
+        driver.findElement(By.id("address_phone")).sendKeys("8625747878");
+        driver.findElement(By.id("address_interest_climb")).click();
+        driver.findElement(By.id("address_interest_dance")).click();
+        driver.findElement(By.id("address_note")).sendKeys("Never Back Down");
+        
+
+
+
+
+
     }
 }
