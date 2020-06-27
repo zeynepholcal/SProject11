3804629743575c59da4d1ffaf13aee49c3682fe2
hale <olcalzeynephale@gmail.com>
2020-06-27 10:21:08 -0400

    project

---
+++ src/ReadMe.java
@@ -0,0 +1,82 @@
+
+public class ReadMe {
+
+    /*
+    NOTE: Create your own account in the website in manual part
+
+    Navigate to website http://a.testaddressbook.com/sign_in
+
+    Click on sign up button
+
+    Automation part
+        Navigate to http://a.testaddressbook.com/sign_in
+
+        Enter the username and password
+
+        Click on sign in button
+
+        Click on Addresses
+
+        Click on new address button
+
+        Enter the first name "Fernando"
+
+        Enter the last name  "Torres"
+
+        Enter the address 1 "Anfield Rd"
+
+        Enter the address 2 "Anfield"
+
+        Enter the city "Liverpool"
+
+        Choose the random option from the state dropdown
+
+        Enter the Zipcode "L4 0TH"
+
+        Click on United States
+
+        Enter the birthday "03/20/1984"
+
+        Enter the age "36"
+
+        Enter the WebSite "https://www.google.com/"
+
+        Enter the phone "8625747878"
+
+        Click on Climbing and Dancing
+
+        Enter the Note "Never Back Down"
+
+        Click on Create Address button
+
+        Verify the first name as "Fernando"
+
+        Verify the last name as "Torres"
+
+        Click on Addresses
+
+        Click on Edit button
+
+        Change the first name as "Peter"
+
+        Change the last name as "Crouch"
+
+        Click on Update address
+
+        Verify the first name as "Peter"
+
+        Verify the last name as "Crouch"
+
+        Click on Addresses
+
+        Click on Destroy
+
+        Address should be removed
+
+
+     */
+
+
+
+
+}
